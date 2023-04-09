5b94a00c18c39dae1c3f27373b383e5c6543666a
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-09 19:57:15 -0300

    :sparkles: feat: add Role to User

--- src/main/java/com/fernandes/authspringsecurity/modules/user/entities/User.java
+++ src/main/java/com/fernandes/authspringsecurity/modules/user/entities/User.java
@@ -1,17 +1,24 @@
 package com.fernandes.authspringsecurity.modules.user.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
 @Table(name = "tb_user")
 @Data
 public class User {
+
+    @Id
+    @GeneratedValue
     private UUID id;
     private String name;
     private String userName;
     private String password;
+    private Set<Role> roles;
 }
