e025a16df46cd994cab0eeeb9bc502e0197d4433
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-10 00:32:22 -0300

    :label: chore: add jakarta annotations to Role

--- src/main/java/com/fernandes/authspringsecurity/modules/user/entities/Role.java
+++ src/main/java/com/fernandes/authspringsecurity/modules/user/entities/Role.java
@@ -1,9 +1,6 @@
 package com.fernandes.authspringsecurity.modules.user.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.UUID;
@@ -16,6 +13,8 @@ public class Role {
     @Id
     @GeneratedValue
     private UUID id;
+
+    @Column(unique = true)
     private String name;
 
     public Role(UUID id) {
