aeae24111f324f1d6ef92075f59dc637497467a9
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-10 17:56:23 -0300

    :sparkles: feat: ProductMapper created

---
+++ src/main/java/com/fernandes/authspringsecurity/modules/product/services/mapper/ProductMapper.java
@@ -0,0 +1,13 @@
+package com.fernandes.authspringsecurity.modules.product.services.mapper;
+
+import com.fernandes.authspringsecurity.modules.product.entities.Product;
+import com.fernandes.authspringsecurity.modules.product.model.request.ProductRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mapping(target = "id", ignore = true)
+    Product toProductEntity(ProductRequest productRequest);
+}
