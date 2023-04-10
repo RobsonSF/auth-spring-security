38fc6e77be5793a36cf1cf3f1f3909cd01461f32
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-10 00:25:45 -0300

    :sparkles: feat: implemented CreateUserService to CreateUserServiceImpl

---
+++ src/main/java/com/fernandes/authspringsecurity/modules/user/services/impl/CreateUserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.fernandes.authspringsecurity.modules.user.services.impl;
+
+import com.fernandes.authspringsecurity.modules.user.repositories.UserRepository;
+import com.fernandes.authspringsecurity.modules.user.entities.User;
+import com.fernandes.authspringsecurity.modules.user.model.UserRequest;
+import com.fernandes.authspringsecurity.modules.user.services.CreateUserService;
+import com.fernandes.authspringsecurity.modules.user.services.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateUserServiceImpl implements CreateUserService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Autowired
+    UserMapper mapper;
+
+    @Override
+    public User execute(UserRequest userRequest) {
+        var user = mapper.toUserEntity(userRequest);
+        return repository.save(user);
+    }
+}
