97e060b4ec243e56d7d93c795db28be5306c8aec
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-13 13:49:31 -0300

    :sparkles: feat: add BCryptPasswordEncoder

--- src/main/java/com/fernandes/authspringsecurity/modules/user/services/impl/CreateUserServiceImpl.java
+++ src/main/java/com/fernandes/authspringsecurity/modules/user/services/impl/CreateUserServiceImpl.java
@@ -1,11 +1,12 @@
 package com.fernandes.authspringsecurity.modules.user.services.impl;
 
-import com.fernandes.authspringsecurity.modules.user.repositories.UserRepository;
 import com.fernandes.authspringsecurity.modules.user.entities.User;
 import com.fernandes.authspringsecurity.modules.user.model.UserRequest;
+import com.fernandes.authspringsecurity.modules.user.repositories.UserRepository;
 import com.fernandes.authspringsecurity.modules.user.services.CreateUserService;
 import com.fernandes.authspringsecurity.modules.user.services.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,9 +18,14 @@ public class CreateUserServiceImpl implements CreateUserService {
     @Autowired
     private UserMapper mapper;
 
+    private BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public User execute(UserRequest userRequest) {
         var user = mapper.toUserEntity(userRequest);
+        user.setPassword(passwordEncoder().encode(user.getPassword()));
         return repository.save(user);
     }
 }
