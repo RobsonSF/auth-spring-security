2846c8cc936ebc7f49789e93b23cce952a4ced54
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-16 14:12:41 -0300

    :sparkles: feat: add endpoint findAll to ProductController

--- src/main/java/com/fernandes/authspringsecurity/modules/product/controllers/ProductController.java
+++ src/main/java/com/fernandes/authspringsecurity/modules/product/controllers/ProductController.java
@@ -3,23 +3,32 @@ package com.fernandes.authspringsecurity.modules.product.controllers;
 import com.fernandes.authspringsecurity.modules.product.entities.Product;
 import com.fernandes.authspringsecurity.modules.product.model.request.ProductRequest;
 import com.fernandes.authspringsecurity.modules.product.services.CreateProductService;
+import com.fernandes.authspringsecurity.modules.product.services.FindAllProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
     @Autowired
-    private CreateProductService service;
+    private CreateProductService createProductService;
+
+    @Autowired
+    private FindAllProductService findAllProductService;
 
     @PostMapping("/create")
     public ResponseEntity<Product> create(@RequestBody ProductRequest productRequest) {
-        var product = service.execute(productRequest);
+        var product = createProductService.execute(productRequest);
         return ResponseEntity.ok().body(product);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Product>> findAll(Pageable pageable) {
+        var products = findAllProductService.execute(pageable);
+        return ResponseEntity.ok().body(products);
     }
 }
