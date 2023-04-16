1253ed9b378bfc6771b63bd8d72b2c2b3f9b64f4
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-16 15:56:58 -0300

    :truck: chore: swapped User for UserResponse and added mapper

--- src/main/java/com/fernandes/authspringsecurity/modules/user/controllers/UserController.java
+++ src/main/java/com/fernandes/authspringsecurity/modules/user/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.fernandes.authspringsecurity.modules.user.controllers;
 
-import com.fernandes.authspringsecurity.modules.user.entities.User;
-import com.fernandes.authspringsecurity.modules.user.model.RoleToUserRequest;
-import com.fernandes.authspringsecurity.modules.user.model.UserRequest;
+import com.fernandes.authspringsecurity.modules.user.controllers.mapper.UserResponseMapper;
+import com.fernandes.authspringsecurity.modules.user.model.request.RoleToUserRequest;
+import com.fernandes.authspringsecurity.modules.user.model.request.UserRequest;
+import com.fernandes.authspringsecurity.modules.user.model.response.UserResponse;
 import com.fernandes.authspringsecurity.modules.user.services.CreateRoleToUserService;
 import com.fernandes.authspringsecurity.modules.user.services.CreateUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
@@ -19,16 +23,21 @@ public class UserController {
     @Autowired
     private CreateRoleToUserService roleToUserServiceImpl;
 
+    @Autowired
+    private UserResponseMapper mapper;
+
     @PostMapping("/create")
-    public ResponseEntity<User> create(@RequestBody UserRequest userRequest) {
+    public ResponseEntity<UserResponse> create(@RequestBody UserRequest userRequest) {
         var user = service.execute(userRequest);
-        return ResponseEntity.ok().body(user);
+        var userResponse = mapper.toUserResponse(user);
+        return ResponseEntity.ok().body(userResponse);
     }
 
     @PostMapping("/role")
-    public ResponseEntity<User> role(@RequestBody RoleToUserRequest roleToUserRequest) {
+    public ResponseEntity<UserResponse> role(@RequestBody RoleToUserRequest roleToUserRequest) {
         var user = roleToUserServiceImpl.execute(roleToUserRequest);
-        return ResponseEntity.ok().body(user);
+        var userResponse = mapper.toUserResponse(user);
+        return ResponseEntity.ok().body(userResponse);
     }
 
 }
