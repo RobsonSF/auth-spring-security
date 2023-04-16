9cdf9bfff0f49944395bbdb6c8dfac292bdfb3d3
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-16 15:39:20 -0300

    :recycle: refactor: UserMapper and RoleMapper renamed to UserRequestMapper and RoleRequestMapper

--- src/main/java/com/fernandes/authspringsecurity/modules/user/services/mapper/RoleRequestMapper.java
+++ src/main/java/com/fernandes/authspringsecurity/modules/user/services/mapper/RoleRequestMapper.java
@@ -1,13 +1,13 @@
 package com.fernandes.authspringsecurity.modules.user.services.mapper;
 
 import com.fernandes.authspringsecurity.modules.user.entities.Role;
-import com.fernandes.authspringsecurity.modules.user.model.RoleRequest;
+import com.fernandes.authspringsecurity.modules.user.model.request.RoleRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 
-public interface RoleMapper {
+public interface RoleRequestMapper {
 
     @Mapping(target = "id", ignore = true)
     Role toRoleEntity(RoleRequest roleRequest);
--- src/main/java/com/fernandes/authspringsecurity/modules/user/services/mapper/UserRequestMapper.java
+++ src/main/java/com/fernandes/authspringsecurity/modules/user/services/mapper/UserRequestMapper.java
@@ -1,12 +1,12 @@
 package com.fernandes.authspringsecurity.modules.user.services.mapper;
 
 import com.fernandes.authspringsecurity.modules.user.entities.User;
-import com.fernandes.authspringsecurity.modules.user.model.UserRequest;
+import com.fernandes.authspringsecurity.modules.user.model.request.UserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper {
+public interface UserRequestMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "roles", ignore = true)
