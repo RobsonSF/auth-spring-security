dbe552e8c715e18b46d13b8b3f3bb84275b49faf
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-16 15:43:26 -0300

    :recycle: refactor: imports

--- src/main/java/com/fernandes/authspringsecurity/modules/user/controllers/mapper/RoleResponseMapper.java
+++ src/main/java/com/fernandes/authspringsecurity/modules/user/controllers/mapper/RoleResponseMapper.java
@@ -3,7 +3,6 @@ package com.fernandes.authspringsecurity.modules.user.controllers.mapper;
 import com.fernandes.authspringsecurity.modules.user.entities.Role;
 import com.fernandes.authspringsecurity.modules.user.model.response.RoleResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 
