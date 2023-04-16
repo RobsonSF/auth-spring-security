2119a1432beeb67adc18966ea067c92eb90293f0
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-16 15:55:57 -0300

    :truck: chore: swapped Role for RoleResponse and added mapper

--- src/main/java/com/fernandes/authspringsecurity/modules/user/controllers/RoleController.java
+++ src/main/java/com/fernandes/authspringsecurity/modules/user/controllers/RoleController.java
@@ -1,7 +1,8 @@
 package com.fernandes.authspringsecurity.modules.user.controllers;
 
-import com.fernandes.authspringsecurity.modules.user.entities.Role;
-import com.fernandes.authspringsecurity.modules.user.model.RoleRequest;
+import com.fernandes.authspringsecurity.modules.user.controllers.mapper.RoleResponseMapper;
+import com.fernandes.authspringsecurity.modules.user.model.request.RoleRequest;
+import com.fernandes.authspringsecurity.modules.user.model.response.RoleResponse;
 import com.fernandes.authspringsecurity.modules.user.services.CreateRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +18,13 @@ public class RoleController {
     @Autowired
     private CreateRoleService service;
 
+    @Autowired
+    private RoleResponseMapper mapper;
+
     @PostMapping("/create")
-    public ResponseEntity<Role> create(@RequestBody RoleRequest roleRequest) {
+    public ResponseEntity<RoleResponse> create(@RequestBody RoleRequest roleRequest) {
         var role = service.execute(roleRequest);
-        return ResponseEntity.ok().body(role);
+        var roleResponse = mapper.toRoleResponse(role);
+        return ResponseEntity.ok().body(roleResponse);
     }
 }
