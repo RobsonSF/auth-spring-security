c7f85afe159e2283f151ecdf111e123590497bd8
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-16 14:07:10 -0300

    :sparkles: feat: add findByUsername

--- src/main/java/com/fernandes/authspringsecurity/modules/user/repositories/UserRepository.java
+++ src/main/java/com/fernandes/authspringsecurity/modules/user/repositories/UserRepository.java
@@ -2,10 +2,17 @@ package com.fernandes.authspringsecurity.modules.user.repositories;
 
 import com.fernandes.authspringsecurity.modules.user.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username = :username" )
+    Optional<User> findByUsername(@Param("username") String username);
+
 }
