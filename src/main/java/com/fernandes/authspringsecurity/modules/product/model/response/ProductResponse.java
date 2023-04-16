59ae78fe395e1f67e3d75a37714010249b3ae176
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-16 15:31:40 -0300

    :sparkles: feat: ProductResponse created

---
+++ src/main/java/com/fernandes/authspringsecurity/modules/product/model/response/ProductResponse.java
@@ -0,0 +1,8 @@
+package com.fernandes.authspringsecurity.modules.product.model.response;
+
+public record ProductResponse(
+        String id,
+        String name,
+
+        String description
+) {}
